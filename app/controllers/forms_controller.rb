b6abadea06aab49f86d41897eed817a5d5d92f4a
Jason Liu <yordlejason@berkeley.edu>
2019-04-07 20:09:49 -0700

    initialized tutor/student/teacher form controllers

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,10 +1,29 @@
 class FormsController < ApplicationController
+
   def teacher
+      session[:question_number] = params[:inputs]
+      puts(params)
+      if (question_number == 1)
+          if params[:answers][question_numbewr] == "In-class"
+            cur_question_number = 3
+            redirect_to forms/teachers
+          else
+
+          end
+      end
   end
 
   def parent_student
   end
 
   def tutor
+      session[:q_page] = 0
+      @q_page = 0
+      render 'tutor'
+  end
+
+  def tutor_next_q
+      @q_page = session[:q_page] + 1
+      render 'tutor'
   end
 end
