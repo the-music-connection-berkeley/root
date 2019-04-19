dbdf3edf973e1708a624f060968179cd9615ceb2
Hong Seok Jang <hongseok219@gmail.com>
2019-04-19 00:41:33 -0700

    Database finish but Tutor's form submit button doesn't work

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -19,19 +19,70 @@ class FormsController < ApplicationController
       name = params[:question][:teacher_name]
       phone = params[:question][:phone]
       email = params[:question][:email]
+      class_name = params[:question][:class_name]
+      school_name = params[:question][:school_name]
+      grade = params[:question][:grade]
+      time_availavility = params[:question][:time_availavility]
+      start_date = params[:question][:start_date]
+      end_date = params[:question][:end_date]
+      instruments = params[:question][:instruments]
+      comment = params[:question][:comment]
       teacher = Teacher.new
-      teacher.attributes = {name: name, phone: phone, email: email}
+      teacher.attributes = {name: name, phone: phone,
+        email: email, class_name: class_name, school_name: school_name,
+        grade: grade, time_availavility: time_availavility, start_date: start_date,
+      end_date: end_date, instruments: instruments, comment: comment}
       teacher.save!
       render 'thank_you'
   end
 
   def student_submit
-      puts "SUBMIT!"
+    name = params[:question][:name]
+    phone = params[:question][:phone]
+    email = params[:question][:email]
+    address = params[:question][:address]
+    piano = params[:question][:piano]
+    grade = params[:question][:grade]
+    instruments = params[:question][:instruments]
+    experiences = params[:question][:experiences]
+    pastapp = params[:question][:pastapp]
+    lunch = params[:question][:lunch]
+    student = Student.new
+  student.attributes = {name: name, phone: phone,
+      email: email, address: address, piano: piano,
+      grade: grade, instruments: instruments, experiences: experiences,
+    pastapp: pastapp, lunch: lunch}
+    student.save!
       render 'thank_you'
   end
 
   def tutor_submit
-    puts "SUBMIT!"
+    name = params[:question][:name]
+    phone = params[:question][:phone]
+    email = params[:question][:email]
+    sid = params[:question][:sid]
+    year = params[:question][:year]
+    major = params[:question][:major]
+    minor = params[:question][:minor]
+    experiences = params[:question][:experiences]
+    in_class = params[:question][:in_class]
+    instruments = params[:question][:instruments]
+    time_availavility = params[:question][:time_availavility]
+    preferred_grade = params[:question][:preferred_grade]
+    private = params[:question][:private]
+    piano = params[:question][:piano]
+    returning = params[:question][:returning]
+    prev_again = params[:question][:prev_again]
+    interview_time = params[:question][:interview_time]
+
+    tutor = Tutor.new
+    tutor.attributes = {name: name, phone: phone,
+      email: email, sid: sid, year: year,
+      major: major, minor: minor, experiences: experiences,
+    in_class: in_class, instruments: instruments, time_availavility: time_availavility,
+   preferred_grade: preferred_grade, private: private, piano: piano, returning: returning,
+   prev_again: prev_again, interview_time: interview_time}
+    tutor.save!
     render 'thank_you'
   end
 
--- db/migrate/20190410043642_create_tutors.rb
+++ db/migrate/20190410043642_create_tutors.rb
@@ -4,6 +4,20 @@ class CreateTutors < ActiveRecord::Migration
       t.string :name
       t.string :phone
       t.string :email
+      t.string :sid
+      t.string :year
+      t.string :major
+      t.string :minor
+      t.string :experiences
+      t.string :in_class
+      t.string :instruments
+      t.string :time_availavility
+      t.string :preferred_grade
+      t.string :private
+      t.string :piano
+      t.string :returning
+      t.string :prev_again
+      t.string :interview_time
       t.timestamps null: false
     end
   end
